7e1a976d4e1b3b4d8a8933d8c7949dd2f1f99393
vishal Kumar <vishalworkplace89@gmail.com>
2026-06-30 05:57:58 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 echo "================="
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${GITHUB_EMAIL}"
-git config --global u --add safe.directory /github/workspace
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config --global --add safe.directory /github/workspace
 
 python /usr/bin/feed.py
 
 
-git add -A && git commit -m "Update Feed"
-git puch --set-upstream origin main 
+git add -A 
+git commit -m "Update Feed" || echo "No change to commit"
+git push --set-upstream origin main 
 
 echo "================="
